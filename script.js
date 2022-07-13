1390e1905dba4559033fa410f17727a7c5146565
user <example@example.com>
2022-07-13 19:31:13 -0400

    test

---
+++ script.js
@@ -0,0 +1,75 @@
+var time = +prompt('Введите время');
+switch (time) {
+case 6: 
+        alert(time + ' часа ночи')
+        break;
+case 5: 
+        alert(time + ' часа ночи')
+        break;
+case 4: 
+        alert(time + ' часа ночи')
+        break;
+case 3: 
+        alert(time + ' часа ночи')
+        break;
+case 2: 
+        alert(time + ' часа ночи')
+        break;
+case 1: 
+        alert(time + ' часа ночи')
+        break;
+case 0: 
+        alert(time + ' часа ночи')
+        break;
+case 7: 
+        alert(time + ' часа утра')
+        break;
+case 8: 
+        alert(time + ' часа утра')
+        break;
+case 9: 
+        alert(time + ' часа утра')
+        break;
+case 10: 
+        alert(time + ' часа утра')
+        break;
+case 11: 
+        alert(time + ' часа дня')
+        break;
+case 12: 
+        alert(time + ' часа дня')
+        break;
+case 13: 
+        alert(time + ' часа дня')
+        break;
+case 14: 
+        alert(time + ' часа дня')
+        break;
+case 15: 
+        alert(time + ' часа дня')
+        break;
+case 16: 
+        alert(time + ' часа дня')
+        break;
+case 17: 
+        alert(time + ' часа дня')
+        break;
+case 18: 
+        alert(time + ' часа вечера')
+        break;
+case 19: 
+        alert(time + ' часа вечера')
+        break;
+case 20: 
+        alert(time + ' часа вечера')
+        break;
+case 21: 
+        alert(time + ' часа вечера')
+        break; 
+case 22: 
+        alert(time + ' часа вечера')
+        break; 
+case 23: 
+        alert(time + ' часа вечера')
+        break;         
+}
